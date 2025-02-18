2ec4fcb4995940b571bf482ecc99525d93a18f7b
adrien <adrien.mille42@gmail.com>
2025-02-18 16:04:29 +0100

    Modification du constructeur

--- src/Entity.hpp
+++ src/Entity.hpp
@@ -8,87 +8,59 @@ class Entity
         std::vector<std::unique_ptr<Component>> components;
 
     public:
-        Entity(std::vector<std::unique_ptr<Component>>) 
-        {
-
+        // 🌟 Constructeur simplifié : ajoute plusieurs composants directement
+        template <typename... TArgs>
+        Entity(TArgs&&... args) {
+            (addComponent(std::forward<TArgs>(args)), ...);
         }
 
-
+        // 🌟 Version originale de `addComponent`
         template <typename T, typename... TArgs>
-        T& addComponent(TArgs&&... mArgs)
-        {
-            T* c(new T(std::forward<TArgs>(mArgs)...));
+        T& addComponent(TArgs&&... mArgs) {
+            // Création dynamique du composant avec les arguments passés
+            T* c = new T(std::forward<TArgs>(mArgs)...);
             c->entity = this;
-            std::unique_ptr<Component>uPtr { c };
+            std::unique_ptr<Component> uPtr { c };
             components.emplace_back(std::move(uPtr));
 
-            componentArray[getComponentTypeID<T>()] = c;
-            componentBitset[getComponentTypeID<T>()] = true;
-
+            // Initialisation du composant
             c->init();
             return *c;
         }
 
+        void update()
+        {
+            for (auto& c : components) c->update();
+        }
+        void draw() 
+        {
+            for (auto& c : components) c->draw();
+        }
 
+        bool isActive() const { return active; }
+        
+        void destroy() { active = false; }
 
+        bool hasGroup(Group mGroup)
+        {
+            return groupBitset[mGroup];
+        }
 
+        void addGroup(Group mGroup);
 
+        void delGroup(Group mGroup)
+        {
+            groupBitset[mGroup] = false;
+        }
 
+        template <typename T> bool hasComponent() const
+        {
+            return componentBitset[getComponentTypeID<T>()];
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	void update()
-	{
-		for (auto& c : components) c->update();
-	}
-	void draw() 
-	{
-		for (auto& c : components) c->draw();
-	}
-
-	bool isActive() const { return active; }
-	void destroy() { active = false; }
-
-	bool hasGroup(Group mGroup)
-	{
-		return groupBitset[mGroup];
-	}
-
-	void addGroup(Group mGroup);
-	void delGroup(Group mGroup)
-	{
-		groupBitset[mGroup] = false;
-	}
-
-	template <typename T> bool hasComponent() const
-	{
-		return componentBitset[getComponentTypeID<T>()];
-	}
-
-	
-
-	template<typename T> T& getComponent() const
-	{
-		auto ptr(componentArray[getComponentTypeID<T>()]);
-		return *static_cast<T*>(ptr);
-	}
+        template<typename T> T& getComponent() const
+        {
+            auto ptr(componentArray[getComponentTypeID<T>()]);
+            return *static_cast<T*>(ptr);
+        }
 };
