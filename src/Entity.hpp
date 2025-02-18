6113d43c2706231af61a432ff771748af5a2bbee
adrien <adrien.mille42@gmail.com>
2025-02-18 16:15:33 +0100

    Modif structure entity

--- src/Entity.hpp
+++ src/Entity.hpp
@@ -1,11 +1,54 @@
+#include <iostream>
 #include <vector>
 #include <memory>
-#include "Component.hpp"
+#include <algorithm>
+#include <bitset>
+#include <array>
+
+using ComponentID = std::size_t;
+using Group = std::size_t;
+
+inline ComponentID getNewComponentTypeID()
+{
+	static ComponentID lastID = 0u;
+	return lastID++;
+}
+
+template <typename T> inline ComponentID getComponentTypeID() noexcept
+{
+	static_assert (std::is_base_of<Component, T>::value, "");
+	static ComponentID typeID = getNewComponentTypeID();
+	return typeID;
+}
+
+constexpr std::size_t maxComponents = 32;
+constexpr std::size_t maxGroups = 32;
+
+using ComponentBitSet = std::bitset<maxComponents>;
+using GroupBitset = std::bitset<maxGroups>;
+
+using ComponentArray = std::array<Component*, maxComponents>;
+
+class Component
+{
+public: 
+	Entity* entity;
+
+	virtual void init() {}
+	virtual void update() {}
+	virtual void draw() {}
+	virtual ~Component() {}
+};
 
 class Entity
 {
     private:
+        bool active = true;
         std::vector<std::unique_ptr<Component>> components;
+
+        ComponentArray componentArray;
+        ComponentBitSet componentBitset;
+        GroupBitset groupBitset;
 
     public:
         // 🌟 Constructeur simplifié : ajoute plusieurs composants directement
@@ -23,9 +66,23 @@ class Entity
             std::unique_ptr<Component> uPtr { c };
             components.emplace_back(std::move(uPtr));
 
+            componentArray[getComponentTypeID<T>()] = c;
+            componentBitset[getComponentTypeID<T>()] = true;
+
             // Initialisation du composant
             c->init();
             return *c;
+        }
+
+        template <typename T> bool hasComponent() const
+        {
+            return componentBitset[getComponentTypeID<T>()];
+        }
+
+        template<typename T> T& getComponent() const
+        {
+            auto ptr(componentArray[getComponentTypeID<T>()]);
+            return *static_cast<T*>(ptr);
         }
 
         void update()
@@ -38,7 +95,7 @@ class Entity
         }
 
         bool isActive() const { return active; }
-        
+
         void destroy() { active = false; }
 
         bool hasGroup(Group mGroup)
@@ -51,16 +108,5 @@ class Entity
         void delGroup(Group mGroup)
         {
             groupBitset[mGroup] = false;
-        }
-
-        template <typename T> bool hasComponent() const
-        {
-            return componentBitset[getComponentTypeID<T>()];
-        }
-
-        template<typename T> T& getComponent() const
-        {
-            auto ptr(componentArray[getComponentTypeID<T>()]);
-            return *static_cast<T*>(ptr);
         }
 };
