40e0bee9e858e2c0dc06f47b8b3a36c8dd725875
adrien <adrien.mille42@gmail.com>
2025-02-18 17:34:16 +0100

    Ajout de TransfromComponent

---
+++ src/TransformComponent.hpp
@@ -0,0 +1,51 @@
+#include "Entity.hpp"
+
+class Vector2 
+{
+    private:
+        float x = 0;
+        float y = 0;
+
+    public:
+        float getX() const { return x; }
+        float getY() const { return y; }
+
+        void setX(float x_) { x = x_; }
+        void setY(float y_) { y = y_; }
+};
+
+class TransformComponent : public Component
+{
+public:
+	Vector2 position;
+    Vector2 direction; 
+
+	int height = 32;
+	int width = 32;
+	int scale = 1;
+
+	int speed = 3;
+
+	bool blocked = false;
+
+	TransformComponent(int sc)
+	{
+		scale = sc;
+	}
+
+	TransformComponent(float x, float y, int h, int w, int sc)
+	{
+		position.setX(x);
+		position.setY(y);
+		height = h;
+		width = w;
+		scale = sc;
+	}
+
+	void update() override
+	{
+		position.setX(position.getX()+direction.getX()*speed);
+		position.setY(position.getY()+direction.getY()*speed);
+	}
+
+};
